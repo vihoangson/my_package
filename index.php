937f897e85fa42805a29665716a7678092448d7c
[Lampart] Vi Hoang Son <hoang_son@lampart-vn.com>
2021-03-24 13:52:46 +0700

    update

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 include_once './vendor/autoload.php';
 $o = new Lampart\Lib\Repositories\Room\RoomRepositoryOptimize;
-$o->mmm();$o->mmm();
+$o->mmm();$o->mmm();$o->mmm();$o->mmm();$o->mmm();
 var_dump($o);
 //echo $o->greet('23');
